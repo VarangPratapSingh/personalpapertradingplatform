a086f2a36ba31c7ec0148099b4d8e43386394e11
Varang Pratap Singh <varangpratapsingh@Varangs-MacBook-Air.local>
2026-03-22 12:25:52 +0530

    fix backend port config for deployment

--- papertradingplatform/backend/server.js
+++ papertradingplatform/backend/server.js
@@ -129,4 +129,7 @@ app.get("/api/user/me", authMiddleware, async(req, res)=>{
     res.json(user);
 })
 
-app.listen(5001,()=>console.log("Server Running On 5001"));
+const PORT = process.env.PORT || 5001;
+app.listen(PORT,()=>{
+    console.log(`Server Running On ${PORT}`);
+});
